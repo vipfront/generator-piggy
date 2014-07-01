fb82397a01ed820256d4acf2a39f5f7540e784a5
tom.wang <tom.wang.sz@qq.com>
2014-07-01 20:42:23 +0800

    newpage支持选择模块
    newpage支持选择模块

--- newpage/index.js
+++ newpage/index.js
@@ -4,6 +4,7 @@ var path = require('path');
 var yeoman = require('yeoman-generator');
 var yosay = require('yosay');
 var chalk = require('chalk');
+var utils = require('../lib/utils');
 
 var NewpageGenerator = yeoman.generators.Base.extend({
   init: function () {
@@ -17,25 +18,35 @@ var NewpageGenerator = yeoman.generators.Base.extend({
     // Have Yeoman greet the user.
     this.log(yosay('Happy Piggy!'));
 
-    // TODO: 询问新页面放到哪个模块下
+    var subModules = utils.getSubModules();
+
+    // 询问新页面放到哪个模块下
     var prompts = [
         {
             type: 'input',
             name: 'pageName',
             message: 'Input your Page\'s name.'
+        },
+        {
+            type: 'list',
+            name: 'moduleName',
+            message: 'Which Sub-Module?',
+            choices: subModules
         }
     ];
 
     this.prompt(prompts, function (props) {
       this.pageName = props.pageName;
+      // 直接返回的是数组中的值
+      this.moduleName = props.moduleName;
 
       done();
     }.bind(this));
   },
 
   files: function () {
-    this.template('_index.html', 'src/html/main/<%= pageName %>.html');
-    this.template('_index.js', 'src/js/main/<%= pageName %>.js');
+    this.template('_index.html', 'src/html/<%= moduleName %>/<%= pageName %>.html');
+    this.template('_index.js', 'src/js/<%= moduleName %>/<%= pageName %>.js');
   }
 });
 
